2d84953b8eab6df4325940637096506dd851f6f9
selam24 <109312381+selam24@users.noreply.github.com>
2024-10-30 10:16:36 -0400

    Update axios.js

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://selam-backend-evangadi.onrender.com/api",
+  // baseURL: "https://selam-backend-evangadi.onrender.com/api",
+   baseURL: "https://backend-evangadi-wlxy.onrender.com/api",
 });
 export default instance;
